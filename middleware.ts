ce63b7f1f6ed84120345b3d8cb0fdba5c75938d8
malewis5 <matthew.a.lewis7@gmail.com>
2025-05-08 11:05:23 -0400

    remove debugging log.

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  console.log(request.nextUrl.pathname);
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
   const cspHeader = `
     default-src 'self';
